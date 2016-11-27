e99e47c345cde6856467a17c44e7f7a948dc1a7e
smanda7 <sivamanda@gmail.com>
2016-11-27 12:24:25 -0600

    Create ClientMain.java

---
+++ src/assignment7/ClientMain.java
@@ -0,0 +1,19 @@
+/* Chatroom ClientMain.java
+ * EE422C Project 7 submission by
+ * Aaron Chang
+ * AAC3434
+ * 16475
+ * Siva Manda
+ * SM48525
+ * 16480
+ * Slip days used: <0>
+ * Git URL: https://github.com/aaronachang/Project7
+ * Fall 2016
+ */
+package assignment7;
+
+public class ClientMain {
+	public static void main(String[] args) {
+		
+	}
+}
