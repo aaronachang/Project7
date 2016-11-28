17e5f047d8088e6269487ca1e776eb65936c59f7
aaronachang <aaron.chang@utexas.edu>
2016-11-27 19:48:56 -0600

    refactor Client to ClientMain

--- src/assignment7/ClientMain.java
+++ src/assignment7/ClientMain.java
@@ -13,7 +13,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane; 
 import javafx.stage.Stage; 
 
-public class Client extends Application { 
+public class ClientMain extends Application { 
 	private TextArea incoming;
 	private TextField outgoing;
 	private BufferedReader reader;
